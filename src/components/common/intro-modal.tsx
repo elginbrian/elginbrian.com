373ffa0b51e4feb62957af622cd4c76b89478f43
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-12-23 16:39:57 +0700

    change(modal): change the modal text

--- src/components/common/intro-modal.tsx
+++ src/components/common/intro-modal.tsx
@@ -35,42 +35,11 @@ export default function Modal({ isOpen, onClose }: ModalProps) {
 
         <div className="text-white">
           <div className="flex flex-col gap-8 items-center">
-            <img src="https://i.gifer.com/17xo.gif" alt="Meme" width={120} height={20} />
+            <img src="https://i.gifer.com/17xo.gif" alt="Meme" width={100} height={20} />
 
             <ul className="text-sm text-center font-[family-name:var(--font-geist-mono)]">
-              <li className="mb-2">This website is still under development.</li>
-              <li className="mb-2">by: Elgin Brian Wahyu Bramadhika</li>
-              <li>For now you can access my socials if you want to know more.</li>
+              <li>This website is currently under development. Some features may not be fully functional yet.</li>
             </ul>
-            <div className="flex gap-4 items-center flex-row">
-              <a
-                className="rounded-full border border-solid border-white transition-colors flex items-center justify-center hover:bg-gray-700 hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                href="https://linkedin.com/in/elginbrian"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaLinkedin className="text-xl sm:text-2xl text-white sm:mr-2" />
-                <span className="hidden sm:block">LinkedIn</span>
-              </a>
-              <a
-                className="rounded-full border border-solid border-white transition-colors flex items-center justify-center hover:bg-gray-700 hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                href="https://github.com/elginbrian"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaGithub className="text-xl sm:text-2xl text-white sm:mr-2" />
-                <span className="hidden sm:block">GitHub</span>
-              </a>
-              <a
-                className="rounded-full border border-solid border-white transition-colors flex items-center justify-center hover:bg-gray-700 hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                href="https://www.instagram.com/_elginbrian/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaInstagram className="text-xl sm:text-2xl text-white sm:mr-2" />
-                <span className="hidden sm:block">Instagram</span>
-              </a>
-            </div>
           </div>
         </div>
       </div>
